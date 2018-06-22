c9d858e640d16966828c92f089b424383d6f8bba
TCJ23 <tcj2012@gmail.com>
2018-06-22 08:50:32 +0200

    hackerrank30days of code

---
+++ src/main/java/Abstract/MyBook.java
@@ -0,0 +1,9 @@
+package Abstract;
+
+public class MyBook extends Book{
+
+    @Override
+    void setTitle(String s) {
+        super.title = s;
+    }
+}
---
+++ src/main/java/Abstract/Soccer.java
@@ -0,0 +1,22 @@
+package Abstract;
+
+class Soccer extends Sports {
+    @Override
+    String getName() {
+        return "Soccer Class";
+    }
+
+    @Override
+    void getNumberOfTeamMembers() {
+        System.out.println("Each team has 11 players in Soccer Class");
+    }
+
+    public static void main(String[] args) {
+        Sports c1 = new Sports();
+        Soccer c2 = new Soccer();
+        System.out.println(c1.getName());
+        c1.getNumberOfTeamMembers();
+        System.out.println(c2.getName());
+        c2.getNumberOfTeamMembers();
+    }
+}
