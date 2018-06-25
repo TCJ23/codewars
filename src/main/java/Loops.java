aa39d9f726806ab1c0c10c2e5cba15f84705d733
TCJ23 <tcj2012@gmail.com>
2018-06-25 17:05:08 +0200

    hackerrank30days of code

---
+++ src/main/java/Loops.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Loops {
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        do {
+
+            for (int i = 1; i <= 10; i++) {
+                System.out.println( n + " x " + i + " = " + i * n);
+            }
+        } while (n > 2 && n < 20);
+        scanner.close();
+    }
+}
