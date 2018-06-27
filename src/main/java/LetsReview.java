4d3f897c02cf327b7cee2e99609bed40aa429a44
TCJ23 <tcj2012@gmail.com>
2018-06-27 18:10:58 +0200

    niemcy

--- src/main/java/LetsReview.java
+++ src/main/java/LetsReview.java
@@ -7,29 +7,29 @@ public class LetsReview {
         Scanner scanner = new Scanner(System.in);
         int t = scanner.nextInt();
         scanner.nextLine();
+
         if (1 <= t && t <= 10) {
             for (int i = 0; i < t; i++) {
                 String s1 = scanner.nextLine();
                 char[] myCharArray = s1.toCharArray();
-                if (myCharArray.length < 10000 && myCharArray.length > 2) {
-                    for (int j = 0; j < myCharArray.length; j++) {
-                        if (j % 2 == 0) {
-                            System.out.print(myCharArray[j]);
-                        }
-                    }
-                    System.out.print(" ");
 
-                    for (int j = 0; j < myCharArray.length; j++) {
-                        if (j % 2 != 0) {
-                            System.out.print(myCharArray[j]);
-                        }
+                for (int j = 0; j < myCharArray.length; j++) {
+                    if (j % 2 == 0) {
+                        System.out.print(myCharArray[j]);
                     }
-                    System.out.println();
-                    scanner.close();
                 }
+                System.out.print(" ");
+
+                for (int j = 0; j < myCharArray.length; j++) {
+                    if (j % 2 != 0) {
+                        System.out.print(myCharArray[j]);
+                    }
+                }
+                System.out.println();
             }
         } else {
             System.out.println("T must be between 1 and 10");
         }
+        scanner.close();
     }
 }
